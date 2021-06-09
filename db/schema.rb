c3026c466430d1c53c18f2b8e57f6b8694a30e88
Flavie Leroy <leroy.flavie@outlook.fr>
2021-06-09 13:01:19 +0200

    in progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_102117) do
+ActiveRecord::Schema.define(version: 2021_06_09_104914) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2021_06_09_102117) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
